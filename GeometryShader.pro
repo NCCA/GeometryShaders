9a2f68c7f9e65eeb6c338d1c4d8c1427a88c03a1
jmacey <jmacey@bournemouth.ac.uk>
2015-10-07 15:15:23 +0100

    Updated for the latest OpenGL Window

--- GeometryShader.pro
+++ GeometryShader.pro
@@ -4,7 +4,6 @@ TARGET=GeometryShader
 OBJECTS_DIR=obj
 # core Qt Libs to use add more here if needed.
 QT+=gui opengl core
-
 # as I want to support 4.8 and 5 this will set a flag for some of the mac stuff
 # mainly in the types.h file for the setMacVisual which is native in Qt5
 isEqual(QT_MAJOR_VERSION, 5) {
@@ -16,9 +15,10 @@ MOC_DIR=moc
 # on a mac we don't create a .app bundle file ( for ease of multiplatform use)
 CONFIG-=app_bundle
 # Auto include all .cpp files in the project src directory (can specifiy individually if required)
-SOURCES+= $$PWD/src/*.cpp
+SOURCES+= $$PWD/src/NGLScene.cpp    \
+					$$PWD/src/main.cpp
 # same for the .h files
-HEADERS+= $$PWD/include/*.h
+HEADERS+= $$PWD/include/NGLScene.h
 # and add the include dir into the search path for Qt and make
 INCLUDEPATH +=./include
 # where our exe is going to live (root of project)
@@ -44,40 +44,12 @@ CONFIG += console
 	# now add it as an extra target
 	QMAKE_EXTRA_TARGETS += first copydata
 }
-# use this to suppress some warning from boost
-QMAKE_CXXFLAGS_WARN_ON += "-Wno-unused-parameter"
-# basic compiler flags (not all appropriate for all platforms)
-QMAKE_CXXFLAGS+= -msse -msse2 -msse3
-macx:QMAKE_CXXFLAGS+= -arch x86_64
-macx:INCLUDEPATH+=/usr/local/include/
-linux-g++:QMAKE_CXXFLAGS +=  -march=native
-linux-g++-64:QMAKE_CXXFLAGS +=  -march=native
-# define the _DEBUG flag for the graphics lib
-DEFINES +=NGL_DEBUG
-
-unix:LIBS += -L/usr/local/lib
-# add the ngl lib
-unix:LIBS +=  -L/$(HOME)/NGL/lib -l NGL
-
-# now if we are under unix and not on a Mac (i.e. linux)
-linux-*{
-		linux-*:QMAKE_CXXFLAGS +=  -march=native
-		DEFINES += LINUX
+NGLPATH=$$(NGLDIR)
+isEmpty(NGLPATH){ # note brace must be here
+	message("including $HOME/NGL")
+	include($(HOME)/NGL/UseNGL.pri)
 }
-DEPENDPATH+=include
-# if we are on a mac define DARWIN
-macx:DEFINES += DARWIN
-# this is where to look for includes
-INCLUDEPATH += $$(HOME)/NGL/include/
-
-win32: {
-        PRE_TARGETDEPS+=C:/NGL/lib/NGL.lib
-        INCLUDEPATH+=-I c:/boost
-        DEFINES+=GL42
-        DEFINES += WIN32
-        DEFINES+=_WIN32
-        DEFINES+=_USE_MATH_DEFINES
-        LIBS += -LC:/NGL/lib/ -lNGL
-        DEFINES+=NO_DLL
+else{ # note brace must be here
+	message("Using custom NGL location")
+	include($(NGLDIR)/UseNGL.pri)
 }
-
--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -20,7 +20,7 @@ const static float INCREMENT=0.01;
 //----------------------------------------------------------------------------------------------------------------------
 const static float ZOOM=0.1;
 
-NGLScene::NGLScene(QWindow *_parent) : OpenGLWindow(_parent)
+NGLScene::NGLScene()
 {
   // re-size the widget to that of the parent (in this case the GLFrame passed in on construction)
   m_rotate=false;
@@ -36,28 +36,22 @@ NGLScene::NGLScene(QWindow *_parent) : OpenGLWindow(_parent)
 
 NGLScene::~NGLScene()
 {
-  ngl::NGLInit *Init = ngl::NGLInit::instance();
   std::cout<<"Shutting down NGL, removing VAO's and Shaders\n";
-
-  Init->NGLQuit();
 }
 
-void NGLScene::resizeEvent(QResizeEvent *_event )
+void NGLScene::resizeGL(int w, int h)
 {
-  if(isExposed())
-  {
-  int w=_event->size().width();
-  int h=_event->size().height();
-  // set the viewport for openGL
-  glViewport(0,0,w,h);
+  // set the viewport for openGL we need to take into account retina display
+  // etc by using the pixel ratio as a multiplyer
+  glViewport(0,0,w*devicePixelRatio(),h*devicePixelRatio());
   // now set the camera size values as the screen size has changed
-  m_cam->setShape(45,(float)w/h,0.05,350);
-  renderLater();
-  }
+  m_cam->setShape(45.0f,(float)width()/height(),0.05f,350.0f);
+  update();
 }
 
 
-void NGLScene::initialize()
+
+void NGLScene::initializeGL()
 {
   // we must call this first before any other GL commands to load and link the
   // gl commands from the lib, if this is not done program will crash
@@ -191,7 +185,7 @@ void NGLScene::loadMatricesToNormalShader()
 
 }
 
-void NGLScene::render()
+void NGLScene::paintGL()
 {
   // clear the screen and depth buffer
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -240,7 +234,7 @@ void NGLScene::mouseMoveEvent (QMouseEvent * _event)
     m_spinYFace += (float) 0.5f * diffx;
     m_origX = _event->x();
     m_origY = _event->y();
-    renderLater();
+    update();
 
   }
         // right mouse translate code
@@ -252,7 +246,7 @@ void NGLScene::mouseMoveEvent (QMouseEvent * _event)
     m_origYPos=_event->y();
     m_modelPos.m_x += INCREMENT * diffX;
     m_modelPos.m_y -= INCREMENT * diffY;
-    renderLater();
+    update();
 
    }
 }
@@ -308,7 +302,7 @@ void NGLScene::wheelEvent(QWheelEvent *_event)
 	{
 		m_modelPos.m_z-=ZOOM;
 	}
-	renderLater();
+	update();
 }
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -342,5 +336,5 @@ void NGLScene::keyPressEvent(QKeyEvent *_event)
 
 
   }
- renderLater();
+ update();
 }
